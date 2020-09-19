11cfedf354c5cc5eeba1b423b3b2beddb45e4099
Matthew McArthur <matthew.mcarthur91@gmail.com>
2020-09-19 19:45:28 -0300

    should probably use vcs

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(calfw lsp-treemacs lsp-ivy lsp-ui which-key vue-mode use-package undo-tree rustic multi-vterm lsp-mode doom-themes counsel centered-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#E5E9F0")))))
---
+++ init.el
@@ -0,0 +1,1 @@
+(org-babel-load-file (expand-file-name "~/.config/emacs/my-init.org"))
