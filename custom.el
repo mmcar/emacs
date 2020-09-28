ecfe862da0dbf9b4a2afece4a8d5b402ee2c5731
Matthew McArthur <matthew.mcarthur91@gmail.com>
2020-09-28 14:06:00 -0300

    Mon 28 Sep 2020 02:06:00 PM ADT

--- custom.el
+++ custom.el
@@ -3,11 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
  '(package-selected-packages
-   '(calfw lsp-treemacs lsp-ivy lsp-ui which-key vue-mode use-package undo-tree rustic multi-vterm lsp-mode doom-themes counsel centered-window)))
+   '(pdf-tools restclient yasnippet company which-key vue-mode use-package undo-tree rustic multi-vterm lsp-ui lsp-treemacs lsp-ivy doom-themes counsel centered-window calfw))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 143 :width normal))))
  '(fringe ((t (:background "#E5E9F0")))))
